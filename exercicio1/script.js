007dbc40e9fcd0d5f5eca7c885a136901d4925a9
Michelle Lang Baratto <mi.baratto@gmail.com>
2022-11-28 20:05:17 +0100

    exercicio strings 1

---
+++ exercicio1/script.js
@@ -0,0 +1,10 @@
+const nome = prompt("Olá, qual seu nome?")
+const comidaFavoriata1 = prompt(`${nome}, quero as suas top 3 comidas favoritas.
+Digite sua comida favorita número 1:`)
+const comidaFavoriata2 = prompt(`Digite sua comida favorita número 2:`)
+const comidaFavoriata3 = prompt(`Digite sua comida favorita número 3:`)
+
+console.log(`Essas são suas comidas favoritas, ${nome}
+${comidaFavoriata1},
+${comidaFavoriata2},
+${comidaFavoriata3}`)
